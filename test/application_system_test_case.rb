43882de67d4cb619409b7d08c0b5be9be7187c0b
minhtienvu <minhtienvu2210@gmail.com>
2022-03-03 08:55:10 +0700

    Initialize project

---
+++ test/application_system_test_case.rb
@@ -0,0 +1,6 @@
+require "test_helper"
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  # We can use the headless_chrome driver instead to prevent the Google Chrome window from opening:
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+end
